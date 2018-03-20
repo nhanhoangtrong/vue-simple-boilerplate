2cc5e8c92197c87d84aba84b80b8ec4754daf7af
Nhan Hoang <siege250294@gmail.com>
2018-03-20 18:09:31 +0700

    chore: Setup testing with Mocha and Karma

---
+++ test/World.spec.js
@@ -0,0 +1,12 @@
+import { shallow } from '@vue/test-utils';
+import { expect } from 'chai';
+import World from '../src/components/World.vue';
+
+describe('Test World component', () => {
+    it('Render World with suitable content and name', () => {
+        const wrapper = shallow(World);
+
+        expect(wrapper.find('h2').text()).to.be.equal('World!');
+        expect(wrapper.name()).to.be.eq('world');
+    });
+});
