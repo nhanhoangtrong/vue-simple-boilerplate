a6448dab87d6f8e8d432e6d9e332cab2499a5f46
Nhan Hoang <siege250294@gmail.com>
2018-04-06 13:01:33 +0700

    feat: Sync Vuex store with routes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Vuex from 'vuex';
 import VueApollo from 'vue-apollo';
+import { sync } from 'vuex-router-sync';
 import routes from './routes';
 import App from './App';
 import storeConfig from './storeConfig';
@@ -11,13 +12,17 @@ Vue.use(VueRouter);
 Vue.use(Vuex);
 Vue.use(VueApollo);
 
+const store = new Vuex.Store(storeConfig);
+const router = new new VueRouter({ mode: 'history', routes })();
+sync(store, router);
+
 new Vue({
     el: '#app',
     render(h) {
         return h(App);
     },
-    store: new Vuex.Store(storeConfig),
-    router: new VueRouter({ mode: 'history', routes }),
+    store,
+    router,
     provide: new VueApollo({
         defaultClient: apolloClient,
     }).provide(),
