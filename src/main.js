46bbda82c44c5f4c53cb43439bdb2fb742598d78
Nhan Hoang <siege250294@gmail.com>
2018-04-06 12:54:00 +0700

    feat: Sync router with vuex store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Vuex from 'vuex';
+import { sync } from 'vuex-router-sync';
 import routes from './routes';
 import App from './App';
 import storeConfig from './storeConfig';
@@ -8,11 +9,15 @@ import storeConfig from './storeConfig';
 Vue.use(VueRouter);
 Vue.use(Vuex);
 
+const store = new Vuex.Store(storeConfig);
+const router = new VueRouter({ mode: 'history', routes });
+sync(store, router);
+
 new Vue({
     el: '#app',
     render(h) {
         return h(App);
     },
-    store: new Vuex.Store(storeConfig),
-    router: new VueRouter({ mode: 'history', routes }),
+    store,
+    router,
 });
